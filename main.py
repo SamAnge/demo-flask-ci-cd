84f26d27d3ce441849ff1cf2e753c56f534cf9cf
samy237 <sbalepa@gmail.com>
2019-11-28 01:18:59 +0100

    fix indentation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-"""This module does blah blah."""
+""
+"This module does blah blah."
+""
 
 from flask import Flask
 
@@ -6,7 +8,7 @@ APP = Flask(__name__)
 
 @APP.route('/home')
 def home():
-  """return greetings."""
-  return 'Hello World'
+    """return greetings."""
+return 'Hello World'
 
-APP.run(host='0.0.0.0', port=5000)
+APP.run(host = '0.0.0.0', port = 5000)
